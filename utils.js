95e9cf192fae358f6b8f264e8374666e3dfeee21
Anton Osmond <antonosmond@gmail.com>
2018-05-09 16:19:15 +0100

    lint utils

--- utils.js
+++ utils.js
@@ -8,29 +8,29 @@ function getType(method) {
 function lookupServiceMetadata(service, implementation) {
     const lowercase = coll => {
         return coll.map(k => {
-            return k.toLowerCase()
-        })
-    }
+            return k.toLowerCase();
+        });
+    };
     const serviceKeys = Object.keys(service);
     const implementationKeys = Object.keys(implementation);
     const intersectingMethods = lowercase(serviceKeys)
-                    .filter(k => lowercase(implementationKeys).indexOf(k) !== -1)
-                    .reduce((acc, k) => {
-                        const method = service[k];
-                        const components = method.path.split('/') 
-                        acc[k] = {
-                            name: components[1],
-                            method: components[2],
-                            type: getType(method), 
-                        };
-                        return acc;
-                    }, {});
+        .filter(k => lowercase(implementationKeys).indexOf(k) !== -1)
+        .reduce((acc, k) => {
+            const method = service[k];
+            const components = method.path.split('/'); 
+            acc[k] = {
+                name: components[1],
+                method: components[2],
+                type: getType(method), 
+            };
+            return acc;
+        }, {});
 
     return key => Object.keys(intersectingMethods)
-                    .filter(k => key.toLowerCase() === k)
-                    .map(k => intersectingMethods[k]).pop();
+        .filter(k => key.toLowerCase() === k)
+        .map(k => intersectingMethods[k]).pop();
 }
 
 module.exports = {
     lookupServiceMetadata,
-}
+};
