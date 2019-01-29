9869c8d11b84b957667a72fc237fb8b47b2fafae
RobinHoodie <robin@oreon.io>
2019-01-29 11:41:13 +0100

    Add types

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare module '@echo-health/grpc-interceptors' {
+  import grpc from 'grpc';
+
+  function serverProxy(server: grpc.Server): ServerProxy;
+
+  export interface Context {
+    call: {
+      request: any;
+      domain?: string;
+      metadata: grpc.Metadata;
+      cancelled: boolean;
+    };
+    service: any;
+  }
+
+  export interface Client extends grpc.Server {
+    interceptors: { (...args: any): any }[];
+    use: (
+      middlewareFn: ((
+        context: Context,
+        next: (...args: any) => Promise<void>,
+        error: (error: grpc.ServiceError) => void
+      ) => void)
+    ) => void;
+  }
+
+  export interface ServerProxy extends Client {
+    intercept: () => IterableIterator<{ fn: (...args: any) => any }>;
+  }
+
+  export { serverProxy };
+}
